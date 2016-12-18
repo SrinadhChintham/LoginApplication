a91225d41fd13846ba27417aff1e3309763b78a7
SRINADH <SRINADH@LAPTOP-HTMKTLMQ>
2016-12-18 15:16:28 -0600

    commentis added by me

--- LoginApplication/src/Sample.java
+++ LoginApplication/src/Sample.java
@@ -1,6 +1,7 @@
 
 public class Sample {
 public static void main(String[] args) {
+	//System.out.println("Login Details");
 	String first = "first";
 	   String second = new String("first");
 	   "first".concat("second");
