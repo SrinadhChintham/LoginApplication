aee729b109dce223535d05169965ca43c1879d46
SRINADH <SRINADH@LAPTOP-HTMKTLMQ>
2016-12-26 23:08:33 -0500

    Added two samole comments

--- LoginApplication/src/Sample.java
+++ LoginApplication/src/Sample.java
@@ -12,9 +12,10 @@ public static void main(String[] args) {
 	   System.out.println("Hello I am Srinadh writing this application");
 	   //added a sop statement
 	   System.out.println("this is my application");
-	  
+	   //test 0
 	   //test 1
 	   //test 2
+	   //test 3
 	   System.out.println(first.equals(second));
 	   System.out.println(first == second);
 	   System.out.println(first.equals("firstsecond"));
